a0a9ff6774264a075fbbf7872933edd25880c0dc
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-02-18 19:37:53 +0900

    feat: 인증에 대한 시간초 표시

--- frontend/src/components/popups/PasswordResetPopup.jsx
+++ frontend/src/components/popups/PasswordResetPopup.jsx
@@ -15,13 +15,26 @@ const PasswordResetPopup = ({ isOpen, onClose }) => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isPasswordMatch, setIsPasswordMatch] = useState(true);
 
+  const [timeLeft, setTimeLeft] = useState(300); // 5분(300초)
+  const [isCodeExpired, setIsCodeExpired] = useState(false);
+
   useEffect(() => {
     setIsEmailValid(emailPattern.test(email));
 
     const isPasswordValid = password === confirmPassword && password !== "";
     setIsPasswordMatch(isPasswordValid);
+  }, [email, emailPattern, emailCode, password, confirmPassword]);
 
-  }, [email, emailPattern, emailCode, password, confirmPassword, isPasswordMatch]);
+  useEffect(() => {
+    if (timeLeft > 0 && isCodeVisible) {
+      const timer = setInterval(() => {
+        setTimeLeft((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    } else if (timeLeft === 0) {
+      setIsCodeExpired(true); // 타이머 만료 시
+    }
+  }, [timeLeft, isCodeVisible]);
 
   if (!isOpen) return null;
 
@@ -34,7 +47,6 @@ const PasswordResetPopup = ({ isOpen, onClose }) => {
       openChangePassword();
       alert(response.data);
     } catch (error) {
-      console.log(error);
       alert(error.response.data.message);
     }
   };
@@ -48,8 +60,6 @@ const PasswordResetPopup = ({ isOpen, onClose }) => {
     const data = { email: email, password: password, emailCode: emailCode };
     try {
       const response = await axios.patch("/auth/change-password", data);
-      console.log(response);
-
       alert(response.data);
       onClose();
     } catch (error) {
@@ -59,6 +69,7 @@ const PasswordResetPopup = ({ isOpen, onClose }) => {
 
   function openChangePassword() {
     setIsCodeVisible(true);
+    setTimeLeft(300); // 타이머 시작
     setPassword("");
     setConfirmPassword("");
   }
@@ -66,7 +77,24 @@ const PasswordResetPopup = ({ isOpen, onClose }) => {
   function cancelPasswordChange() {
     setIsCodeVisible(false);
     setEmail("");
+    setIsCodeExpired(false);
   }
+
+  // 이메일 포맷을 숨기기 위한 함수
+  const formatEmail = (email) => {
+    const parts = email.split("@");
+    const localPart = parts[0];
+    const domainPart = parts[1];
+
+    return `${localPart.slice(0, 5)}*****@${domainPart}`;
+  };
+
+  // 시간 포맷팅 함수
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
 
   return (
     <div className={styles.popupOverlay} style={{ zIndex: 1001 }}>
@@ -78,92 +106,95 @@ const PasswordResetPopup = ({ isOpen, onClose }) => {
         <div className={styles.popupContent}>
           {!isCodeVisible && (
             <>
-            <div className={styles.infoRow}>비밀번호를 찾기위한 이메일을 입력해주세요.</div>
-            <div className={styles.infoRow}>
-              <input
-                type="email"
-                name="email"
-                placeholder="example@nicesite.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <Button
-                variant={isEmailValid ? "green" : "disabled"}
-                size="input"
-                onClick={handleEmailVerification}
-                disabled={!isEmailValid || isCodeVisible}
-                type="button"
-              >
-                Send
-              </Button>
-            </div>
+              <div className={styles.infoRow}>비밀번호를 찾기위한 이메일을 입력해주세요.</div>
+              <div className={styles.infoRow}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="example@nicesite.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <Button
+                  variant={isEmailValid ? "green" : "disabled"}
+                  size="input"
+                  onClick={handleEmailVerification}
+                  disabled={!isEmailValid || isCodeVisible}
+                  type="button"
+                >
+                  Send
+                </Button>
+              </div>
             </>
           )}
           {isCodeVisible && (
             <>
-            <div className={styles.infoRow}>
-              <label htmlFor="email">Email:</label>
-              <p>{email}</p>
-            </div>
-            <div className={styles.infoRow}>
-              <label htmlFor="email-verification">Email code:</label>
-              <input
-                type="text"
-                id="email-verification"
-                value={emailCode}
-                onChange={(e) => setEmailCode(e.target.value)}
-                placeholder="Enter verification code"
-                required
-              />
-            </div>
-            <div className={styles.infoRow}>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter new password"
-                required
-              />
-            </div>
-            <div className={styles.infoRow}>
-              <label htmlFor="confirmPassword">Confirm Password:</label>
-              <input
-                type="password"
-                id="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                placeholder="Confirm new password"
-                required
-              />
-            </div>
-            <div className={styles.passwordText}>
-              <small>
-                Use 8-20 characters with letters, numbers and symbols.<br/>
-              </small>
-              {!isPasswordMatch && (
-                <small className={styles.passwordError}>Passwords do not match</small>
-              )}
-            </div>
-            <div className={styles.infoRow}>
-              <Button
-                variant="green" size="medium"
-                onClick={handlePasswordChange}
-              >
-                Change Password
-              </Button>
-              <Button
-                variant="logout" size="medium"
-                onClick={cancelPasswordChange}
-              >
-                Cancel
-              </Button>
-            </div>
+              <div className={styles.infoRow}>
+                <label htmlFor="email">Email:</label>
+                <p>{formatEmail(email)}</p>
+              </div>
+              <div className={styles.infoRow}>
+                <label htmlFor="email-verification">Email code:</label>
+                <input
+                  type="text"
+                  id="email-verification"
+                  value={emailCode}
+                  onChange={(e) => setEmailCode(e.target.value)}
+                  placeholder={`Enter verification code (${formatTime(timeLeft)})`} // 타이머와 함께 표시
+                  required
+                  disabled={isCodeExpired} // 코드 만료되면 비활성화
+                />
+                {isCodeExpired && <p className={styles.error}>The code has expired. Please request a new one.</p>}
+              </div>
+              <div className={styles.infoRow}>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter new password"
+                  required
+                />
+              </div>
+              <div className={styles.infoRow}>
+                <label htmlFor="confirmPassword">Confirm Password:</label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  placeholder="Confirm new password"
+                  required
+                />
+              </div>
+              <div className={styles.passwordText}>
+                <small>
+                  Use 8-20 characters with letters, numbers, and symbols.<br/>
+                </small>
+                {!isPasswordMatch && (
+                  <small className={styles.passwordError}>Passwords do not match</small>
+                )}
+              </div>
+              <div className={styles.infoRow}>
+                <Button
+                  variant="green" size="medium"
+                  onClick={handlePasswordChange}
+                  disabled={isCodeExpired} // 코드 만료되면 비활성화
+                >
+                  Change Password
+                </Button>
+                <Button
+                  variant="logout" size="medium"
+                  onClick={cancelPasswordChange}
+                >
+                  Cancel
+                </Button>
+              </div>
             </>
           )}
-        </div>  {/* <div className={styles.popupContent}> */}
+        </div>
       </div>
     </div>
   );
