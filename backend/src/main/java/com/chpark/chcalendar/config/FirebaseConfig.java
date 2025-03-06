681c52c9cf5e57a8c971462693e922468ed0f582
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-03-06 15:22:57 +0900

    feat: 테스트 코드에서는 firebase가 초기화되지 않도록
    젠킨스 배포시에 .env파일이 없으므로 firebase 경로가 찾아지지 않는다.

--- backend/src/main/java/com/chpark/chcalendar/config/FirebaseConfig.java
+++ backend/src/main/java/com/chpark/chcalendar/config/FirebaseConfig.java
@@ -5,12 +5,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Profile("!test")
 @Configuration
 public class FirebaseConfig {
 
--- backend/src/test/java/com/chpark/chcalendar/repository/user/UserRepositoryTest.java
+++ backend/src/test/java/com/chpark/chcalendar/repository/user/UserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
