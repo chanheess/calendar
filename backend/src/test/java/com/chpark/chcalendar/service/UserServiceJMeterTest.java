25d75fc85b014f1978e27592a3b9f3d9bd5d8724
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-05-20 13:15:23 +0900

    test: 부하 테스트 코드 추가

---
+++ backend/src/test/java/com/chpark/chcalendar/service/UserServiceJMeterTest.java
@@ -0,0 +1,86 @@
+package com.chpark.chcalendar.service;
+
+import com.chpark.chcalendar.DotenvInitializer;
+import com.chpark.chcalendar.dto.UserDto;
+import com.chpark.chcalendar.entity.UserEntity;
+import com.chpark.chcalendar.repository.user.UserRepository;
+import com.chpark.chcalendar.service.user.UserService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+@Disabled
+@SpringBootTest
+@ContextConfiguration(initializers = DotenvInitializer.class)
+public class UserServiceJMeterTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    String password = "qwer1234!";
+
+    @Test
+    void createUser() {
+        int count = 10000;
+        UserDto.RegisterRequest userDto = new UserDto.RegisterRequest("","","","");
+        for (int i = 0; i < count; i++) {
+            String randomId = getRandom(4) + i;
+
+            userDto.setEmail("test" + randomId + "@gmail.com");
+            userDto.setPassword(password);
+            userDto.setNickname("tester" + randomId);
+
+            userService.create(userDto);
+        }
+    }
+
+    @Test
+    void generateUserCsvForJMeter() throws IOException {
+        String outputFile = "/Users/chpark/users.csv"; // 경로는 JMeter 실행 위치에 맞게 조정
+        int userCount = 1000;
+
+        List<UserEntity> userList = userRepository.findByNicknameContaining("tester");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            // 헤더 작성
+            writer.write("email,password");
+            writer.newLine();
+
+            // 예: testUser001 ~ testUser100
+            for (int i = 0; i <= userCount; i++) {
+                if (userList.size() <= i) {
+                    break;
+                }
+
+                String email = userList.get(i).getEmail();
+                String password = this.password;    //평문으로 넣어야 테스트 가능
+
+                writer.write(email + "," + password);
+                writer.newLine();
+            }
+        }
+
+        System.out.println("✅ users.csv 파일 생성 완료: " + outputFile);
+    }
+
+    public static String getRandom(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+}
