94c40b36a258695525a78cf084087285bd2a84d2
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-06-04 11:25:59 +0900

    fix: 토큰 발급후 재요청하도록 수정

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,25 +1,48 @@
 import axios from 'axios';
 
+let refreshPromise = null;
+let requestQueue = [];
+
 axios.interceptors.response.use(
   response => response,
   async error => {
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && error.response.data === "jwtToken expired" && !originalRequest._retry) {
-      originalRequest._retry = true;
+    if (
+      error.response?.status === 401 &&
+      error.response.data === "jwtToken expired" &&
+      !originalRequest._retry
+    ) {
+      if (!refreshPromise) {
+        refreshPromise = axios.post('/auth/refresh')
+          .then(res => {
+            const newToken = res.data.accessToken;
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + newToken;
 
-      try {
-        const refreshResponse = await axios.post('/auth/refresh');
-        const newAccessToken = refreshResponse.data.accessToken;
+            // 모든 대기 중인 요청 재처리
+            requestQueue.forEach(cb => cb(newToken));
+            requestQueue = [];
 
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + newAccessToken;
-        originalRequest.headers['Authorization'] = 'Bearer ' + newAccessToken;
-
-        return axios(originalRequest);
-      } catch (refreshError) {
-        window.location.href = "/auth/login";
-        return Promise.reject(refreshError);
+            return newToken;
+          })
+          .catch(err => {
+            requestQueue = [];
+            window.location.href = "/auth/login";
+            throw err;
+          })
+          .finally(() => {
+            refreshPromise = null;
+          });
       }
+
+      // 새 Promise를 만들어서 큐에 등록
+      return new Promise((resolve, reject) => {
+        requestQueue.push(token => {
+          originalRequest._retry = true;
+          originalRequest.headers['Authorization'] = 'Bearer ' + token;
+          axios(originalRequest).then(resolve).catch(reject);
+        });
+      });
     }
 
     return Promise.reject(error);
