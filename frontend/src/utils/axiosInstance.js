5213379f2ab3c023c6f89533d7be09e868df623c
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-07-18 17:00:50 +0900

    fix: httponly로 동작하도록 수정

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -10,20 +10,14 @@ axios.interceptors.response.use(
 
     if (
       error.response?.status === 401 &&
-      error.response.data === "jwtToken expired" &&
       !originalRequest._retry
     ) {
       if (!refreshPromise) {
         refreshPromise = axios.post('/auth/refresh', null, { withCredentials: true })
-          .then(res => {
-            const newToken = res.data.accessToken;
-            axios.defaults.headers.common['Authorization'] = 'Bearer ' + newToken;
-
+          .then(() => {
             // 모든 대기 중인 요청 재처리
-            requestQueue.forEach(cb => cb(newToken));
+            requestQueue.forEach(cb => cb());
             requestQueue = [];
-
-            return newToken;
           })
           .catch(err => {
             requestQueue = [];
@@ -37,9 +31,10 @@ axios.interceptors.response.use(
 
       // 새 Promise를 만들어서 큐에 등록
       return new Promise((resolve, reject) => {
-        requestQueue.push(token => {
+        requestQueue.push(() => {
           originalRequest._retry = true;
-          originalRequest.headers['Authorization'] = 'Bearer ' + token;
+          // withCredentials 옵션을 반드시 true로 설정
+          originalRequest.withCredentials = true;
           axios(originalRequest).then(resolve).catch(reject);
         });
       });
