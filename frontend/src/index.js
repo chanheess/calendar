785f47b0dedad3d7f9172c37355471220522630c
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-09-07 20:03:57 +0900

    fix: 무한 리다이렉트 문제 재수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'utils/axiosInstance';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-axios.get('/auth/check')
-  .then(() => {
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  })
-  .catch(() => {
-    window.location.href = "/auth/login";
-  });
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
